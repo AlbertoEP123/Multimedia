0cbcc2dc1401583a83d190ebde8ce15300a92ad2
AlbertoEP123 <116571236+AlbertoEP123@users.noreply.github.com>
2023-09-28 13:35:39 +0200

    act

---
+++ Ejercicio9.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  final prueba = Coche("toyota", "auris", 2);
+  print(prueba);
+}
+
+class Coche {
+  String marca="";
+  String modelo="";
+  int id=0;
+
+  Coche(String marca,String modelo,int id){
+    this.marca = marca;
+    this.modelo=modelo;
+    this.id=id;
+    
+  }
+  
+    String get Marca{
+          return marca;
+  }
+    set Marca(String laMarca){
+      marca = laMarca;
+    }
+    String get Modelo{
+          return modelo;
+  }
+    set Modelo(String elModelo){
+      modelo = elModelo;
+    }
+
+     @override
+     String toString() {
+    return 'Marca: ${marca}, Modelo: ${modelo}, ID: ${id}';
+  }
+}
